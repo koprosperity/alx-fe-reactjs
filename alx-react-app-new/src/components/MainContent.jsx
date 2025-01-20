90d1aac87715dc1973728273e194a63f0ee29210
koprosperity <okaip01@gmail.com>
2025-01-20 12:01:50 +0000

    new react folder integrated with css

---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main style={{ padding: '15px', backgroundColor: '#f0f0f0', color: '#333', lineHeight: '1.5' }}>
+            <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;

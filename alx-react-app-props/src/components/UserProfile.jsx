5d4f2b1cd7ac003359c0aad2453209e3c51b6385
koprosperity <okaip01@gmail.com>
2025-01-20 20:22:50 +0000

    import

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,3 +1,18 @@
+import { useContext } from 'react';
+import UserContext from '../UserContext';
+
+function UserProfile() {
+    const userData = useContext(UserContext);
+
+    return (
+        <div>
+            <h2>{userData.name}</h2>
+            <p>Age: Not Specified</p>
+            <p>Email: {userData.email}</p>
+        </div>
+    );
+}
+
 const UserProfile = (props) => {
     return (
         <div>

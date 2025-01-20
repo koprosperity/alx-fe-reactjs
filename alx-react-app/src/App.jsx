58b9e7f943d23fbfee9f45892af9c0eccd3617ca
koprosperity <okaip01@gmail.com>
2025-01-20 11:41:25 +0000

    userprofile

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -6,6 +6,7 @@ import WelcomeMessage from './components/WelcomeMessage';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import UserProfile from './components/UserProfile'; 
 function App() {
   const [count, setCount] = useState(0)
 
@@ -38,6 +39,9 @@ function App() {
             <Header />
             <MainContent />
             <Footer />
+        </div>
+        <div>
+            <UserProfile name="Kofi" age="25" bio="ALX Student and an Enterpreneur." />
         </div>
     </>
   )

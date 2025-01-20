669c3fb6a3bfa6c8354a0f7395c4a579338095fd
koprosperity <okaip01@gmail.com>
2025-01-20 12:21:42 +0000

    counter app

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,10 @@ import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
 import UserProfile from './components/UserProfile'; 
+import Counter from './components/Counter';
+
+
+
 function App() {
   const [count, setCount] = useState(0)
 
@@ -42,6 +46,9 @@ function App() {
         </div>
         <div>
             <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        </div>
+        <div>
+            <Counter />
         </div>
     </>
   )

270ffb0ad324c3165a48ac240b9d76dafe16c045
koprosperity <okaip01@gmail.com>
2025-01-21 12:13:55 +0000

    edit

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,6 +1,7 @@
 // src/components/RecipeList.jsx
 import React from 'react';
 import { useRecipeStore } from '../recipeStore';
+import { Link } from 'react-router-dom';
 
 const RecipeList = () => {
   const filteredRecipes = useRecipeStore((state) => state.filteredRecipes);

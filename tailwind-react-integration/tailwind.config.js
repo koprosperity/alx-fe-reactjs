d56cc2dc21256efa7ec2ed25ddf2180608394f5d
koprosperity <okaip01@gmail.com>
2025-01-23 00:02:39 +0000

    purge

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   purge: ["./public/index.html"], // Files to scan for Tailwind classes
+  content: [".public/index.html"],
   darkMode: "class", // Enable dark mode via "class" or "media"
   theme: {
     extend: {}, // Extend default Tailwind theme here

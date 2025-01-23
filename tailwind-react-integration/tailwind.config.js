2b9f13a6c8bde495f2931115745992cb7bc1b5e7
koprosperity <okaip01@gmail.com>
2025-01-23 00:00:48 +0000

    pp

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,7 +1,5 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",
-    "./src/main.jsx", "./public/index.html"
-  ], // Files to scan for Tailwind classes
+  purge: ["./public/index.html"], // Files to scan for Tailwind classes
   darkMode: "class", // Enable dark mode via "class" or "media"
   theme: {
     extend: {}, // Extend default Tailwind theme here
